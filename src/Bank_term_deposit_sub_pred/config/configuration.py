e8e20174df647013cf2c91d57336387f0e5986da
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-08-23 20:36:26 +0530

    config entity for data ingestion

---
+++ src/Bank_term_deposit_sub_pred/config/configuration.py
@@ -0,0 +1,5 @@
+from src.Bank_term_deposit_sub_pred.constants import *
+from src.Bank_term_deposit_sub_pred.logging import logger
+from src.Bank_term_deposit_sub_pred.entity.config_entity import DataIngestionConfig
+
+
