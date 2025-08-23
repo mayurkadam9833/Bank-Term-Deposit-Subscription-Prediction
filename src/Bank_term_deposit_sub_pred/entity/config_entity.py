c70f406408027dcf231e242e2b447e648038f5e8
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-08-23 23:32:33 +0530

    add status file path to data validation folder

--- src/Bank_term_deposit_sub_pred/entity/config_entity.py
+++ src/Bank_term_deposit_sub_pred/entity/config_entity.py
@@ -8,3 +8,8 @@ class DataIngestionConfig:
     source_url: str
     local_data_file: Path 
     unzip_dir: Path
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path 
+    unzip_data: Path
