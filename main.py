a4048c78e19c886a99ce6d2e80052f537b067c57
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-08-23 23:01:30 +0530

    create data ingsetion pipeline

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from src.Bank_term_deposit_sub_pred.logging import logger
 from src.Bank_term_deposit_sub_pred.pipeline.stage_01_data_ingestion import DataIngestionPipeline
 
 
-
+# data ingestion stage variable
 stage_one="Data Ingestion"
 
+# data ingestion pipeline
 if __name__ == "__main__":
     try:
         logger.info(f"<<<< stage: {stage_one} started >>>>")
