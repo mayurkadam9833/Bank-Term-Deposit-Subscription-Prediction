59cc0aced05bb061fc08ec8393e136c2bdc90df9
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-08-23 20:27:59 +0530

    saved path for yaml files

---
+++ src/Bank_term_deposit_sub_pred/constants/__init__.py
@@ -0,0 +1,6 @@
+from pathlib import Path 
+
+# yaml file paths for configuration,schema,parameters
+CONFIG_FILE_PATH=Path("config/config.yaml")
+SCHEMA_FILE_PATH=Path("schema.yaml")
+PARAMS_FILE_PATH=Path("params.yaml")
