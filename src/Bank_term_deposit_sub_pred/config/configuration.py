3f540256e0880b5bf5728fb7d44aa7fd865a6d84
mayurkadam9833 <mayurkadam9833@gmail.com>
2025-08-23 21:19:52 +0530

    create config manager for read yaml files(params,schema,config)

--- src/Bank_term_deposit_sub_pred/config/configuration.py
+++ src/Bank_term_deposit_sub_pred/config/configuration.py
@@ -1,5 +1,41 @@
 from src.Bank_term_deposit_sub_pred.constants import *
 from src.Bank_term_deposit_sub_pred.logging import logger
+from src.Bank_term_deposit_sub_pred.utils.common import read_yaml,create_dir
 from src.Bank_term_deposit_sub_pred.entity.config_entity import DataIngestionConfig
 
+"""
+ConfigManager class is responsible for reading config, schema and params yaml files
+and returning dataclass objects for each pipeline stage (ingestion, validation, transformation, training, evaluation)
+"""
+
+class ConfigManager:
+    def __init__(
+        self,
+        config_file_path=CONFIG_FILE_PATH,
+        schema_file_path=SCHEMA_FILE_PATH,
+        params_file_path=SCHEMA_FILE_PATH):
+
+        # read all yaml files (config, schema, params)
+        self.config=read_yaml(config_file_path)
+        self.schema=read_yaml(schema_file_path)
+        self.params=read_yaml(params_file_path)
+
+        # create root artifact directory if not exists
+        create_dir([self.config.artifacts_root])
+
+    # method to get data ingestion config object
+    def get_data_ingestion_config(self)-> DataIngestionConfig:
+        config=self.config.data_ingestion
+
+        # create ingestion folder
+        create_dir([config.root_dir])
+
+        # prepare and return DataIngestionConfig dataclass
+        data_ingsetion_config=DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_url=config.source_url,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir)
+        
+        return data_ingsetion_config
 
